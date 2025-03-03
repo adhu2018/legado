2c1b174ffe1c3b6f0abf961cf04c32d65c44c714
adhu2018 <41824074+adhu2018@users.noreply.github.com>
2025-03-03 12:11:53 +0800

    up

--- app/src/main/java/io/legado/app/utils/FilterUtils.kt
+++ app/src/main/java/io/legado/app/utils/FilterUtils.kt
@@ -1,5 +1,6 @@
 package io.legado.app.utils
 
+import io.legado.app.constant.AppLog
 import io.legado.app.data.appDb
 
 /**
@@ -13,8 +14,10 @@ object FilterUtils {
         filterRules.forEach { item ->
             if (item.pattern.isNotEmpty()) {
                 return if (item.isRegex) {
+                    AppLog.put("filter by regex. \nregex: `${item.pattern}` \nname: $name \nresult: ${item.regex.containsMatchIn(name)}")
                     item.regex.containsMatchIn(name)
                 } else {
+                    AppLog.put("filter by string. \nstring: `${item.pattern}` \nname: $name \nresult: ${item.pattern == name}")
                     item.pattern == name
                 }
             }
